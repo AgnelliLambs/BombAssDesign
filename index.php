20714a9a514e57d9f25794eb605a9f2ff88a6cb5
jkerz48 <jkerz48@gmail.com>
2017-03-29 15:37:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
+<!-- Header PHP-->
 <?php
 	$path = '';
 	$title = 'YouNix';
 	include $path.'assets/header.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Footer PHP -->
+<?php
+	$path = '../';
+	$title = 'YouNiz';
+	include $path.'assets/footer.php';
 ?>
