108f67ff51479c5c8428aa07659c0a77067c85f4
jkerz48 <jkerz48@gmail.com>
2017-03-29 15:20:52 -0400

    Add files via upload

---
+++ assets/footer.php
@@ -0,0 +1,37 @@
+
+	<!-- </div>  end contentwrapper, remove/add if header php is different-->
+
+
+<div id="bottom"> 
+	<div id="bottomtext">
+		<a href="./assets/citations.html" style="color:#b4c1d1;">Citations</a><br>
+		&copy; Bomb Ass Design 2017<br>
+		Phone: 1-800-555-8899 <br>
+		Email: contact@badesign.com<br>
+		1 Lomb Memorial Dr, Rochester, NY 14623 
+	</div> 
+		
+</div> 
+</body> 
+</html> 
+
+
+<!--  CSS associated with footer -->
+<!--
+#bottom{
+	position: fixed; 
+	bottom: 0px; 
+	left: 0px; 
+	width: 100%;
+	height:100px;
+	padding:40px;/*Change height and padding to increase footer size*/
+	background-color:#000;
+}
+#bottomtext{
+	width:998px;
+	margin:0px auto;
+	padding:4px;
+	text-align:center;
+	color:#fffafa;
+}
+-->
