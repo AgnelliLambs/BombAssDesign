dd1b3c92ef316684e45545db8e36ea4bf850d720
Tim Endersby <endersbyt@gmail.com>
2017-03-31 11:48:04 -0400

    Edit footer to not be sticky

--- assets/footer.php
+++ assets/footer.php
@@ -4,8 +4,8 @@
 
 <div id="bottom"> 
 	<div id="bottomtext">
-		<a href="./assets/aboutUs.html" style="color:#b4c1d1;">About Us</a><br>
-		<a href="./assets/citations.html" style="color:#b4c1d1;">Citations</a><br>
+		<a href="<?php echo $path;?>assets/aboutUs.html" style="color:#b4c1d1;">About Us</a><br>
+		<a href="<?php echo $path;?>assets/citations.html" style="color:#b4c1d1;">Citations</a><br>
 		&copy; Bomb Ass Design 2017<br>
 		Phone: 1-800-555-8899 <br>
 		Email: contact@badesign.com<br>
