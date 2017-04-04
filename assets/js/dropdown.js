f4dfa071dfa660c4bb74379545095e0f1bfd230e
Tim Endersby <endersbyt@gmail.com>
2017-04-04 12:02:02 -0400

    fixed caret bug

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,6 +1,5 @@
 $(window).on("load resize",function(e){ //run every time the window is resized
-	var isMobile = /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
-	if (!isMobile || $(window).width() >= 768) {
+	if ($(window).width() >= 768) {
 		document.getElementById("caret").style.display = "inline-block";
 		$(".dropdown").hover(function () {
 			$(this).addClass('open')
