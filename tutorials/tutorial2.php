ab532d25006fe539e99da0a3a0419b5c9b97f40c
bmm723 <bmm7238@rit.edu>
2017-04-05 14:43:40 -0400

    Add files via upload

---
+++ tutorials/tutorial2.php
@@ -0,0 +1,42 @@
+<?php
+	$path = '../';
+	$title = 'Tutorial 1';
+	include $path.'assets/includes/header.php';
+?>
+	<h1>Tutorial 2 - SFTP</h1>
+	
+	<p>
+		SFTP stands for SSH File Transfer Protocol, but is sometimes called Secure File Transfer Protocol. 
+		In this tutorial, you will be learning how to download and use the free software FileZilla. Filezilla
+	</p>
+	
+	<!-- Link to correct computer thing -->
+	
+	<h2>Downloading FileZilla</h2>
+	
+	<p>
+		To begin downloading, please select your operating system from the list below.
+		Clicking the link below will takr you directly to the dowload page on FileZilla.
+	</p>
+	
+	<ol>
+		<li><a href="https://filezilla-project.org/download.php?type=client">Windows 64bit</a></li>
+		<li><a href="https://filezilla-project.org/download.php?platform=win32">Windows 32bit</a></li>
+		<li><a href="https://filezilla-project.org/download.php?platform=osx">Mac OS X</a></li>
+	</ol>
+	
+	<p>Alternatively, you can go to FileZilla's homepage <a href="https://filezilla-project.org/">here</a>.</p>
+	
+	<!--Screenshot of terminal
+		<img src="<\?php echo $path;?>/assets/imgs/filezilla.jpg" alt="FileZilla">
+	-->
+	
+	<h2>TITLE</h2>
+	<!-- Quiz -->
+	
+<!-- Footer PHP-->
+<?php
+	$path = '../';
+	$title = 'Tutorial 1';
+	include $path.'assets/includes/footer.php';
+?>
