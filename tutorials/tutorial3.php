5c83b585e4a187e4edb472d7a1e3b73805a25dda
jkerz48 <jkerz48@gmail.com>
2017-04-05 15:24:22 -0400

    Update tutorial3.php

--- tutorials/tutorial3.php
+++ tutorials/tutorial3.php
@@ -9,7 +9,7 @@
 	include $path.'assets/includes/header.php';
 ?>
 	<h1>Tutorial Three - Creating, Deleting, and Moving Files and Directories</h1>
-	
+	<h2>Directories</h2>
 	<h2>Creating a Directory</h2>
 	<p>mkdir &lt;directoryName&gt;</p>
 	<p>Example: mkdir directory1 </p>
@@ -17,7 +17,7 @@
 	<h2>Removing a Directory </h2>
 	<p>rmdir &lt;directoryName&gt;</p>
 	<p>Example: rmdir directory1 </p>
-	
+	<p>Files</p>
 	<h2>Deleting a File</h2>
 	<p>rm &lt;fileName&gt;</p>
 	<p>Example: rm file1 </p>
