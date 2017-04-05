6cedc4a59e9dc450f048f5a3f1c313535a504fba
jkerz48 <jkerz48@gmail.com>
2017-04-05 14:25:38 -0400

    added comments

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,3 +1,6 @@
+<!-- Header.php-->
+<!-- B.A.D Design -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,16 +9,19 @@
 	<meta charset="utf-8">
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	
 	<!-- Bootstrap -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<!-- Style sheet -->
 	<link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/style.css"> </head>
 
+<!-- PHP link to Navbar -->
 <body>
 	<?php
 	include $path.'assets/includes/navBar.php';
