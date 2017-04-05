039b30f976fd42555f13e57b3a74c5f60f0470ed
jkerz48 <jkerz48@gmail.com>
2017-04-05 15:19:01 -0400

    changed <>'s

--- tutorials/tutorial3.php
+++ tutorials/tutorial3.php
@@ -11,25 +11,27 @@
 	<h1>Tutorial Three - Creating, Deleting, and Moving Files and Directories</h1>
 	
 	<h2>Creating a Directory</h2>
-	<p>mkdir <directoryName></p>
+	<p>mkdir &lt;directoryName&gt;</p>
 	<p>Example: mkdir directory1 </p>
 		
 	<h2>Removing a Directory </h2>
-	<p>rmdir <directoryName></p>
+	<p>rmdir &lt;directoryName&gt;</p>
 	<p>Example: rmdir directory1 </p>
 	
 	<h2>Deleting a File</h2>
-	<p>rm <fileName></p>
+	<p>rm &lt;fileName&gt;</p>
 	<p>Example: rm file1 </p>
 	
 	<h2>Copying a File</h2>
-	<p>cp <fileName1> <fileName2></p>
+	<p>cp &lt;fileName1&gt; &lt;fileName2&gt;</p>
 	<p>Example: cp file1 newFileName</p>
 	
 	<h2>Moving a File</h2>
-	<p>mv <fileName1> <fileName2> <address> </p>	
-	<p>Example: mv file1 file2 assets/includes
+	<p>cp &lt;fileName1&gt; &lt;fileName2&gt;</p>	
+	<p>Example: mv file1 file2 assets/includes</p>
 	<p>can be used to change file name</p>
+		
+	<h2>vi </h2>
 	
 	
 	<!-- Quiz -->
