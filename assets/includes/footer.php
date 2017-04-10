fcb70d76abcf2860b63fec4cbf612afda395a72e
Tim Endersby <endersbyt@gmail.com>
2017-04-10 15:06:36 -0400

    Make footer links clickable

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -7,9 +7,9 @@
 		<a href="<?php echo $path;?>assets/aboutUs.html" style="color:#b4c1d1;">About Us</a><br>
 		<a href="<?php echo $path;?>assets/citations.html" style="color:#b4c1d1;">Citations</a><br>
 		&copy; Bomb Ass Design 2017<br>
-		Phone: 1-800-555-8899 <br>
-		Email: contact@badesign.com<br>
-		1 Lomb Memorial Dr, Rochester, NY 14623 
+		Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br>
+		Email: <a href="mailto:contact@badesign.com? Subject=Website%20contact" target="_top"">contact@badesign.com</a><br>
+		<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
 	</div> 
 		
 </div> 
