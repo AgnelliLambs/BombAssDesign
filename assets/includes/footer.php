f97a23836a3dd84c661eb947d75dd3c0801ca7b9
Tim Endersby <endersbyt@gmail.com>
2017-04-12 11:59:53 -0400

    Seperate mobile and desktop phone links

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -7,7 +7,8 @@
 		<a href="<?php echo $path;?>assets/aboutUs.html" style="color:#b4c1d1;">About Us</a><br>
 		<a href="<?php echo $path;?>assets/citations.html" style="color:#b4c1d1;">Citations</a><br>
 		&copy; Bomb Ass Design 2017<br>
-		Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br>
+		<div id="mobilePhoneLink">Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br></div>
+		<div id="desktopPhone">Phone: 1-800-555-8899</a> <br></div>
 		Email: <a href="mailto:contact@badesign.com? Subject=Website%20contact" target="_top">contact@badesign.com</a><br>
 		<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
 	</div> 
