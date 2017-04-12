90726ed024fda2c52ff4dd5ba6cd984a27ed0bcb
Joseph Agnelli <jxa4717@rit.edu>
2017-04-12 13:23:13 -0400

    There it is
    Added tutorial home page

---
+++ tutorials/tutorial_home.php
@@ -0,0 +1,28 @@
+<!-- Homepage for tutorials, should only appear on mobile -->
+
+<!-- Header PHP-->
+<?php
+	$path = '../';
+	$title = 'Tutorials';
+	$heading = 'Tutorial';
+	include $path.'assets/includes/header.php';
+?>
+<h1>Tutorials</h1>
+
+<div id='content'>
+	<a href = "tutorial1.php">SSH</a> 
+	<a href = "tutorial2.php">Navigation</a> 
+	<a href = "tutorial3.php">Files</a> 
+	<a href = "tutorial4.php">SFTP</a> 
+	<a href = "tutorial5.php">ChMod</a> 
+	<a href = "tutorial6.php">Internet</a> 
+	<a href = "tutorial7.php">Apache</a> 
+	<a href = "tutorial8.php">Text Editors</a> 
+	<div class='median'>
+
+	</div>
+</div> <!-- end of content -->
+<!-- Footer PHP -->
+<?php
+	include $path.'assets/includes/footer.php';
+?>
