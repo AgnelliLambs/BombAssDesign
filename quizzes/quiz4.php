12d43d3826b288c700c8abdba97aac2ad5ce94b7
bmm723 <bmm7238@rit.edu>
2017-04-12 15:05:59 -0400

    Update quiz4.php

--- quizzes/quiz4.php
+++ quizzes/quiz4.php
@@ -7,33 +7,47 @@
 	<div class="quiz">
 		<h1>SFTP Quiz</h1>
 		<form>
-			<h3>Question 1: </h3>
+			<h3>Question 1: Which side of the FileZilla application shows files on your computer?</h3>
 			<select id="4.1">
-				<option> </option>
+				<option>left</option> <!-- Correct answer -->
+				<option>right</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
-			<h3>Question 2: </h3>
+			<h3>Question 2: What does SFTP stand for?</h3>
 			<select id="4.2">
-				<option> </option>
+				<option>Separate File Transferring Protocol</option>
+				<option>Secret File Transmission Protocol </option>
+				<option>Secure File Transfer Protocol</option> <!-- Correct answer -->
+				<option>Secure File Transmission Protocol</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
-			<h3>Question 3: </h3>
+			<h3>Question 3: What is the port number for SFTP?</h3>
 			<select id="4.3">
-				<option> </option>
+				<option>20</option>
+				<option>21</option>
+				<option>22</option> <!-- Correct answer -->
+				<option>23</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
-			<h3>Question 4: </h3>
+			<h3>Question 4: Which side of the FileZilla application shows files on the host?</h3>
 			<select id="4.4">
-				<option> </option>
+				<option>left</option>
+				<option>right</option><!-- Correct answer -->
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
-			<h3>Question 5: </h3>
+			<h3>Question 5: What 4 things do you need to know in order to connect to a file with SFTP?</h3>
 			<select id="4.5">
-				<option> </option>
+				<option>Host name</option> <!-- Correct answer -->
+				<option>IP Address</option> 
+				<option>Directory path</option>
+				<option>Port</option> <!-- Correct answer -->
+				<option>Password</option> <!-- Correct answer -->
+				<option>File name</option>
+				<option>Username</option> <!-- Correct answer -->
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 		</form>
