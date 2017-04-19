68d16565ebbd15b0ebed1e2678152c5a11a2ab51
jkerz48 <jkerz48@gmail.com>
2017-04-19 14:00:18 -0400

    Update citations.php

--- citations.php
+++ citations.php
@@ -7,10 +7,18 @@
 ?>
 
 <h1>Citations</h1>
+<p>
+	Chadwick, R. (n.d.). Linux Tutorial. Retrieved March 08, 2017, from http://ryanstutorials.net/linuxtutorial/<br/>
+
+	Xtensio. (n.d.). Lean Canvas Creator. Retrieved March 08, 2017, from https://xtensio.com/lean-canvas/<br/>
+
+	UNIX Tutorial for Beginners. (2001, October 19). Retrieved March 08, 2017, from http://www.ee.surrey.ac.uk/Teaching/Unix/<br/>
+
+	Wireframe.cc. (n.d.). Minimal wireframing tool. Retrieved March 08, 2017, from https://wireframe.cc/<br/>
+<p>
 
 <!-- Footer PHP -->
 <?php
 	include $path.'assets/includes/footer.php';
 ?>
 
-</html>
