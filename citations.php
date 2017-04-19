b0d7f15a2fcc1c354f95a0a7778f8162230376c9
Tim Endersby <endersbyt@gmail.com>
2017-04-19 10:33:11 -0400

    Create about and Citation pages

---
+++ citations.php
@@ -0,0 +1,16 @@
+<!-- Header PHP-->
+<?php
+	$path = '';
+	$title = 'YouNix';
+	$heading = 'Home';
+	include $path.'assets/includes/header.php';
+?>
+
+<h1>Citations</h1>
+
+<!-- Footer PHP -->
+<?php
+	include $path.'assets/includes/footer.php';
+?>
+
+</html>
