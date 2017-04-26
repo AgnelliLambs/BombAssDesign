a9e2996513a6ac39f3b3dcc71b8125fab0dcc3af
Tim Endersby <endersbyt@gmail.com>
2017-04-26 14:36:07 -0400

    Fix links

--- quizzes/quiz_home.php
+++ quizzes/quiz_home.php
@@ -16,7 +16,7 @@
 	<a href = "quiz3.php">Files</a> 
 	<a href = "quiz4.php">SFTP</a> 
 	<a href = "quiz5.php">Permissions</a> 
-	<a href = "quiz6.php">Internet</a> 
+	<a href = "quiz6.php">Directories</a> 
 	<a href = "quiz7.php">Apache</a> 
 	<a href = "quiz8.php">Text Editors</a> 
 	
--- tutorials/tutorial_home.php
+++ tutorials/tutorial_home.php
@@ -17,7 +17,7 @@
 	<a href = "tutorial3.php">Files</a> 
 	<a href = "tutorial4.php">SFTP</a> 
 	<a href = "tutorial5.php">Permissions</a> 
-	<a href = "tutorial6.php">Internet</a> 
+	<a href = "tutorial6.php">Directories</a> 
 	<a href = "tutorial7.php">Apache</a> 
 	<a href = "tutorial8.php">Text Editors</a> 
 	<div class='median'>
