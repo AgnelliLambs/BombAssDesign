bb01547e441169170730333090587934e4e182bb
jkerz48 <jkerz48@gmail.com>
2017-04-26 15:30:22 -0400

    Update glossary.php

--- glossary.php
+++ glossary.php
@@ -29,41 +29,36 @@
 
 <!-- Commands -->
 <h2>List of Commands</h2>
-	bg &ltjob number&gt - resumes suspended job in the background
 	
-	<p>cd <path> - change directory, changes where your cursor is ‘cd ..’ returns you to the directory that your directory is located in.</p>
+	<p>cd &lt;path&gt; - change directory, changes where your cursor is ‘cd ..’ returns you to the directory that your directory is located in.</p>
 	
-	<p>Chmod <file> <new permissions> - Change the file permissions to <new permissions> refer to Tutorial Four for more.</p>
+	<p>Chmod &lt;file&gt; &lt;new permissions&gt; - Change the file permissions to <new permissions> refer to Tutorial Four for more.</p>
 	
 	clear - clears the text from the Terminal.
 	
-	<p>cp <file1> <file2> - copies a file (file1) to a new location (file2), you can also rename the file.</p>
+	<p>cp &lt;file1&gt; &lt;file2&gt; - copies a file (file1) to a new location (file2), you can also rename the file.</p>
+
 	
-	fg <job number> - resumes a suspended job in foreground
-	
-	Grep <word> <file> - searches for a specific in a file and displays the line it’s in. Adding -i ignores upper/lower cases, using -i’<phrase>’ allows it to search for phrases.
-	
-	kill <job number> - kills a job
-	
+	Grep &lt;word&gt; &lt;file&gt; - searches for a specific in a file and displays the line it’s in. Adding -i ignores upper/lower cases, using -i’<phrase>’ allows it to search for phrases.
+		
 	<p>ls - Lists information about the files in the current directory. -a shows all files, -l shows more information</p>
 	
-	Man <command> - the help command, use this to get more information about a particular command
+	Man &lt;command&gt; - the help command, use this to get more information about a particular command
 	
-	<p>mkdir <directory name> - makes a new directory</p>
+	<p>mkdir &lt;directory name&gt; - makes a new directory</p>
 	
-	<p>mv <file1> <file2>- moves a file (file1) to a new location (file2), this command can also be used to change the file name.</p>
+	<p>mv &lt;file1&gt; &lt;file2&gt; - moves a file (file1) to a new location (file2), this command can also be used to change the file name.</p>
 	
 	<p>pico - opens up a text editor, this can be used to change file content in real time.</p>
 	
 	<p>pwd - Print Working Directory, lists where your cursor is on the computer.</p>
 	
-	<p>rm <filename> - removes the file</p>
+	<p>rm &lt;filename&gt; - removes the file</p>
 	
-	<p>rmdir <directory name> - removes the directory. This would only remove empty directories, to remove nonempty directories, use rmdir -p <filename></p>
-	sleep <seconds> - delays the program by <seconds>
+	<p>rmdir &lt;directory name&gt; - removes the directory. This would only remove empty directories, to remove nonempty directories, use rmdir -p <filename></p>
 	
 	<p>vi - opens up the text editor, this can be used to change file contents.</p>
-	wc <filename> - Gives you a word count of the file. Adding ‘-w’ prints a list of the words, ‘-l’ prints the number of lines.
+	wc &lt;filename&gt; - Gives you a word count of the file. Adding ‘-w’ prints a list of the words, ‘-l’ prints the number of lines.
 
 		</div>
 <!-- Footer PHP-->
