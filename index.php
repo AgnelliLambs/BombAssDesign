60f24117fe6bba26d52287d673551cc137b6af6c
rcb1205 <rcb1205@g.rit.edu>
2017-04-26 14:19:57 -0400

    Switch Home to YouNix

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
 	include $path.'assets/includes/header.php';
 ?>
 		<div class="index">
-			<h1>Home</h1>
+			<h1><span id="iyellow">You</span><span id="ipink">Nix</span></h1>
+			<div class="homeBox">
+			<a href="tutorials/tutorial_home.php"><h1>Tutorials</h1></a>
+			</div>
+			<div class="homeBox"><a href="quizzes/quizzes_home.php">
+			<h1>Quizzes</h1></a>
+			</div>
+			<div class="homeBox"><a href="glossary.php">
+			<h1>Glossary</h1></a>
+			</div>
 		</div>
 
 <!-- Footer PHP -->
