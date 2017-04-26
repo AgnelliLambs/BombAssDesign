b1282bbf777ba1f1a92680e907aae53e2e2fae3e
Tim Endersby <endersbyt@gmail.com>
2017-04-26 14:35:55 -0400

    Add search bar

--- assets/includes/navBar.php
+++ assets/includes/navBar.php
@@ -18,7 +18,7 @@
 					<li><a href="<?php echo $path;?>tutorials/tutorial3.php">Creating and deleting files</a></li>
 					<li><a href="<?php echo $path;?>tutorials/tutorial4.php">SFTP</a></li>
 					<li><a href="<?php echo $path;?>tutorials/tutorial5.php">File Permissions</a></li>
-					<li><a href="<?php echo $path;?>tutorials/tutorial6.php">How the internet works</a></li>
+					<li><a href="<?php echo $path;?>tutorials/tutorial6.php">Directories</a></li>
 					<li><a href="<?php echo $path;?>tutorials/tutorial7.php">Apache</a></li>
 					<li><a href="<?php echo $path;?>tutorials/tutorial8.php">Unix text editors</a></li>
 				</ul>
@@ -31,13 +31,23 @@
 					<li><a href="quizzes/quiz3.php">Creating and deleting files</a></li>
 					<li><a href="quizzes/quiz4.php">SFTP</a></li>
 					<li><a href="quizzes/quiz5.php">File Permissions</a></li>
-					<li><a href="quizzes/quiz6.php">How the internet works</a></li>
+					<li><a href="quizzes/quiz6.php">Directories</a></li>
 					<li><a href="quizzes/quiz7.php">Apache</a></li>
 					<li><a href="quizzes/quiz8.php">Unix text editors</a></li>
 				</ul>
 			</li>
         <li><a href="<?php echo $path;?>glossary.php" id="<?php if ($heading == 'Glossary'){echo 'active';}?>">GLOSSARY</a></li>
       </ul>
+		 <?php if($heading == 'Glossary'){
+			echo '
+		<div class="col-sm-3 col-md-3 pull-right">
+		<form class="navbar-form" role="search">
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+			<div class="input-group-btn">
+				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+			</div>
+			';} ?>
     </div>
   </div>
 </nav>
