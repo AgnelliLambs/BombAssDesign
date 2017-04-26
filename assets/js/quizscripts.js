cb388ed4cc5dfbaa47308d8b3a5277c502e43148
Joseph Agnelli <jxa4717@rit.edu>
2017-04-26 14:47:01 -0400

    changed quiz checking to be better
    maybe...

--- assets/js/quizscripts.js
+++ assets/js/quizscripts.js
@@ -1,10 +1,12 @@
-function checkQuiz1(){
+function checkQuiz(quizNum){
 	
-	var answers = [0,1,2,3]; //a ,b ,c ,d
+	var[][] quizzes;
+	quizzes[0] = [0,1,2,3]; //a ,b ,c ,d
+	quizzes[5]= [1,2,1,1,2];
 	var quizForm = document.getElementsByTagName('form')[0];
 	
-	for(var i = 0; i<answers.length; i++){
-		var ans = document.getElementsByName('answer'+(i+1))[answers[i]];
+	for(var i = 0; i<quizzes[quizNum].length; i++){
+		var ans = document.getElementsByName('answer'+(i+1))[quizzes[quizNum][i]];
 		
 		if(!ans.checked){
 			document.getElementsByClassName('question')[i].style.color = 'red';
