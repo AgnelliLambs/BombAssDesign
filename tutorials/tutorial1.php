39f627e57e5ed9003bff6867b4dc64b8d5e2b918
Tim Endersby <endersbyt@gmail.com>
2017-04-26 15:32:24 -0400

    Update tutorial 1

--- tutorials/tutorial1.php
+++ tutorials/tutorial1.php
@@ -9,19 +9,15 @@
 	include $path.'assets/includes/header.php';
 ?>
 	<div class="tutorial">
-		<h1>SH</h1>
+		<h1>SSH</h1>
 
 		<p>SSH stands for Secure Shell. It is a secure (encrypted) way to remotely log into and control unix computers through the bash command line. In order to control your Unix server from your computer, you'll need to use SSH.</p>
-
-		<!-- Link to correct computer thing -->
 
 		<h2>Open your bash terminal (Mac or linux)</h2>
 
 		<p>Open "Terminal"</p>	
 
-		<!--Screenshot of terminal
-			<img src="<\?php echo $path;?>/assets/imgs/tutorial1/terminal.jpg" alt="Terminal">
-		-->
+			<img src="<?php echo $path;?>/assets/imgs/tutorial1/mac_terminal.png" alt="Mac terminal">
 
 		<p>Use SSH to connect to your Linux machine by typing "SSH" followed by USERNAME@HOSTNAME replacing USERNAME with your account name and replacing HOSTNAME with the hostname or ip address of your linux machine. Press enter to execute the line.</p>
 
@@ -37,6 +33,8 @@
 
 		<p>Since Windows is not based on UNIX, we have to install a program to interface with a UNIX computer from a windows computer.We will use PuTTY to SSH. PuTTY can be downloaded <a href="http://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html">here.</a> </p>
 
+		<img src="<?php echo $path;?>/assets/imgs/tutorial1/Putty_download.png" alt="PuTTY download">
+		
 		<p>When you open putty, enter USERNAME@HOSTNAME into the "Host Name" box, replacing USERNAME with your account name and replacing HOSTNAME with the hostname or ip address of your linux machine. Enter port 22, and then press "Open".</p>
 
 		<img src="<?php echo $path;?>/assets/imgs/tutorial1/putty.jpg" alt="PuTTY">
