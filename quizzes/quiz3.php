2db21bd80e24673c62bacd96c26fef9fc62fe908
jkerz48 <jkerz48@gmail.com>
2017-04-26 14:29:47 -0400

    Update quiz3.php

--- quizzes/quiz3.php
+++ quizzes/quiz3.php
@@ -11,15 +11,15 @@
 	include $path.'assets/includes/header.php';
 ?>
 	<!-- Quiz -->
-	<h2>Files and Directories</h2>
+	<h2>Files</h2>
 	
-	<form>
+		<form>
 			<!-- Question 1 -->
-			<h3 class ="question">Question 1: Which choice is the correct way to create a directory?</h3>
+			<h3 class ="question">Question 1: Which choice is the correct way to create a file?</h3>
 			<select id="3.1">
-				<option>create directory1</option>
-				<option>mkdir directory1</option>
-				<option>vi directory1</option>
+				<option>create file1</option>
+				<option>mkdir file1</option>
+				<option>vi file1</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 		
@@ -34,28 +34,28 @@
 			
 			<!-- Question 3 -->
 			<h3 class ="question">Which choice is the correct way to rename a file?</h3>
-			<select id="3.2">
-				<option>rn file1 file2</option>
-				<option>rmdir file1</option>
-				<option>delete file1</option>
+			<select id="3.3">
+				<option>mv file1 file2</option>
+				<option>rn file1 file 2</option>
+				<option>rename file1 file2</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
 			<!-- Question 4 -->
-			<h3 class ="question">Which choice is ?</h3>
+			<h3 class ="question">Which choice is the correct way move to a file?</h3>
 			<select id="3.4">
-				<option></option>
-				<option></option>
-				<option></option>
+				<option>vi file1 file2 newDestination</option>
+				<option>mv file1 file2 newDestination</option>
+				<option>move file1 file2 newDestination</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 			
 			<!-- Question 5 -->
-			<h3 class ="question">Which choice ?</h3>
+			<h3 class ="question">Which choice is the correct way to copy a file?</h3>
 			<select id="3.5">
-				<option></option>
-				<option></option>
-				<option></option>
+				<option>cp file1 file2</option>
+				<option>copy file1 file2</option>
+				<option>vi file1 file2</option>
 			</select>
 			<input type="button" onclick="checkAnswer()" value="Submit">
 		
