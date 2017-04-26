1f2fbf8b9547d90519d49c39fffc7d73bb9fac2b
rcb1205 <rcb1205@g.rit.edu>
2017-04-26 15:19:57 -0400

    Change quiz to radio buttons

--- quizzes/quiz5.php
+++ quizzes/quiz5.php
@@ -12,47 +12,44 @@
 	<!-- Quiz -->
 	<h2>File Permissions</h2>
 		
-		<form>
+		
 		<!-- Question 1 -->
 			<h3 class ="question">Which command will change a file so it can be read by everyone</h3>
-			<select id="5.1">
-				<option>chmod u+r file</option>
-				<option><!-- Correct -->chmod a+r file</option>
-				<option>chmod a+w file</option>
-			</select>
-			<input type="button" onclick="checkQuiz(5)" value="Submit">
+			<form action="">
+				<input type="radio" name="5.1" value="0">chmod u+r file</br>
+				<input type="radio" name="5.1" value="1"><!-- Correct -->chmod a+r file</br>
+				<input type="radio" name="5.1" value="2">chmod a+w file</br>
+
+
 		<!-- Question 2 -->	
 			<h3 class ="question">Which permission set is typical for any folder</h3>
-			<select id="5.2">
-				<option>-rwxrwxrwx</option>
-				<option>d---------</option>
-				<option><!-- Correct -->drwxr-xr-x</option>
-			</select>
-			<input type="button" onclick="checkQuiz(5)" value="Submit">
+
+				<input type="radio" name="5.2" value="0">-rwxrwxrwx</br>
+				<input type="radio" name="5.2" value="1">drw--wx---</br>
+				<input type="radio" name="5.2" value="2"><!-- Correct -->drwxr-xr-x</br>
+			
 		<!-- Question 3 -->	
 			<h3 class ="question">What command will give execute permission to everyone for meeting?</h3>
-			<select id="5.3">
-				<option>chmod a+r meeting</option>
-				<option><!-- Correect -->chmod a+x meeting</option>
-				<option>chmod uo+x meeting</option>
-			</select>
-			<input type="button" onclick="checkQuiz(5)" value="Submit">
+		
+				<input type="radio" name="5.3" value="0">chmod a+r meeting</br>
+				<input type="radio" name="5.3" value="1"><!-- Correct -->chmod a+x meeting</br>
+				<input type="radio" name="5.3" value="2">chmod uo+x meeting</br>
+
 		<!-- Question 4 -->
 			<h3 class ="question">Your file is not able to be run, what are the permissions set to?</h3>
-			<select id="5.4">
-				<option><!-- wrong -->drw-r--r--</option>
-				<option><!-- Correct -->-rw-r--r--</option>
-				<option><!-- Wrong -->-r-xrwxrwx</option>
-			</select>
-			<input type="button" onclick="checkQuiz(5)" value="Submit">
+			
+				<input type="radio" name="5.4" value="0">drw-r--r--</br>
+				<input type="radio" name="5.4" value="1"><!-- correct -->-rw-r--r--</br>
+				<input type="radio" name="5.4" value="2">-r-xrwxrwx</br>
+
 		<!-- Question 5 -->	
 			<h3 class ="question">What permission numbers would allow the owner to read, write, and execute;
 			group to read, write, and execute; and any user to execute?</h3>
-			<select id="5.5">
-				<option>777</option>
-				<option>141</option>
-				<option><!-- Correct -->771</option>
-			</select>
+			
+				<input type="radio" name="5.5" value="0">777</br>
+				<input type="radio" name="5.5" value="1">141</br>
+				<input type="radio" name="5.5" value="2"><!-- Correct -->771</br>
+
 			<input type="button" onclick="checkQuiz(5)" value="Submit">
 		</form>
 		
