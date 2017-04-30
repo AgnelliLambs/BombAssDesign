6a05bf51663f46edeec3707f8eb1fb8926e63d99
rcb1205 <rcb1205@g.rit.edu>
2017-04-30 19:59:18 -0400

    Fixed Joe quizscripts

--- assets/js/quizscripts.js
+++ assets/js/quizscripts.js
@@ -24,7 +24,7 @@ function checkQuizDeprecated(quizNum){
 }
 
 function checkQuiz(quizNum){
-	quizNum++;
+	quizNum--;
 	var quizzes = [	[0,0,0,0],
 						[0,0,0,0],
 						[0,0,0,0],
@@ -35,13 +35,15 @@ function checkQuiz(quizNum){
 						[0,0,0,0],
 						[0,0,0,0]
 					];
-	for(var i=1;i<=quizzes[quizNum].length;i++){
+	for(var i=0;i<quizzes[quizNum].length;i++){
 		var question = document.getElementsByClassName('question')[i];
-		alert('question');
 		var inputs = question.getElementsByTagName('input');
 		var shouldBeSelected = quizzes[quizNum][i];
 		if(inputs[shouldBeSelected].checked){
-			alert('yay');
+			question.style.color = "green";
+		}
+		else{
+			question.style.color='red';
 		}
 	}
 }
