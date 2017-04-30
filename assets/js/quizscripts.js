02b5316d35b41016339d37aa4eca1bf88d3fd636
Joseph Agnelli <jxa4717@rit.edu>
2017-04-30 19:53:31 -0400

    fixed quizzes

--- assets/js/quizscripts.js
+++ assets/js/quizscripts.js
@@ -1,47 +1,31 @@
-function checkQuizDeprecated(quizNum){
-	quizNum++;
-	var quizzes = [[]];
-	//quizzes[0] = [0,1,2,3]; //a ,b ,c ,d
-	//quizzes[1]= [1,2,1,1,2];
-	//tutorial4 quiz [0,2,2,1,4]
-	var quizForm = document.getElementsByTagName('form')[0];
-	
-	for(var i = 0; i<quizzes[quizNum].length; i++){
-		var ans = document.getElementsByClassName('select')[quizzes[quizNum][i]];
-		
-		var ops = ans.options;
-		
-		
-		if(true){//ops[quizzes[quizNum][i].selected){
-			document.getElementsByClassName('question')[i].style.color = 'black';
-		}
-		else{
-			document.getElementsByClassName('question')[i].style.color = 'red';
-		}
-		//alert (ans.checked ? 'Correct!':'Wrong :(');
-	}
-	
+@@ -23,8 +23,9 @@ function checkQuizDeprecated(quizNum){
 }
 
 function checkQuiz(quizNum){
 	quizNum++;
 	var quizzes = [	[0,0,0,0],
+	quizNum--;
+	var quizzes = [
 						[0,0,0,0],
 						[0,0,0,0],
-						[0,2,2,1,4],
-						[1,2,1,1,2],
 						[0,0,0,0],
 						[0,0,0,0],
+@@ -34,12 +35,15 @@ function checkQuiz(quizNum){
 						[0,0,0,0],
 						[0,0,0,0]
 					];
 	for(var i=1;i<=quizzes[quizNum].length;i++){
+	for(var i=0;i<quizzes[quizNum].length;i++){
 		var question = document.getElementsByClassName('question')[i];
-		alert('question');
 		var inputs = question.getElementsByTagName('input');
 		var shouldBeSelected = quizzes[quizNum][i];
 		if(inputs[shouldBeSelected].checked){
 			alert('yay');
+			question.style.color="green";
+		}
+		else{
+			question.style.color="red";
 		}
 	}
 }
+\ No newline at end of file
