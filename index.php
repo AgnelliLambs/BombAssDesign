e8410be95862bfa2b75b0700a602e0bca9658f70
jkerz48 <jkerz48@gmail.com>
2017-05-03 14:10:54 -0400

    Update index.php
    Took out Twitter :(

--- index.php
+++ index.php
@@ -6,20 +6,7 @@
 	include $path.'assets/includes/header.php';
 ?>
 		<div class="index">
-		<!-- Twitter -->
-		<div class="hidden-sm hidden-xs">	
-		<div class="sidebar-nav-absolute pull-right" style="float:right;">
-               	 <div class="well">
-                         	<a class="twitter-timeline" href="https://twitter.com/therealbadesign" 
-							data-tweet-limit="3"
-							data-width="200px"
-							>
-							Tweets by @therealbadesign</a> 
-							<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-             	   </div>
-                	<!--/.well -->
-			</div>
-          	  </div>
+		
 			<h1 id="logo"><span class="you">You</span><span class="nix">Nix</span></h1>
 			<p>Welcome to YouNix, an interactive UNIX learning experience. Whether you are new to UNIX 
 				or you already have some experience, these tutorials can help you host your website for all 
