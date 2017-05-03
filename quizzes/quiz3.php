91e1b0bdeaa6462843c11af0ad92d6fe1eb8ec7e
jkerz48 <jkerz48@gmail.com>
2017-05-03 15:13:12 -0400

    Update quiz3.php
    fixed some of the tags

--- quizzes/quiz3.php
+++ quizzes/quiz3.php
@@ -14,38 +14,48 @@
 	<form>
 	
 <div class = 'question1'>
-	<p>Question 1: Which choice is the correct way to create a file?</p>
+	<h3>Question 1: Which choice is the correct way to create a file?</h3>
+	<div class ="question">
 	<input type='radio' name='answer1' value='1'>create file1</input>
 	<input type='radio' name='answer1' value='2'>mkdir file1</input>
 	<input type='radio' name='answer1' value='3'>vi file1</input>
+	</div
 </div>
 
 <div class = 'question2'>
-	<p>Which choice is the correct way to delete a file?</p>
+	<div class ="question">
+	<h3>Which choice is the correct way to delete a file?</h3>
 	<input type='radio' name='answer2' value='1'>rm file1</input>
 	<input type='radio' name='answer2' value='2'>rmdir file1</input>
 	<input type='radio' name='answer2' value='3'>delete file1</input>
-</div>
+	</div>
+	</div>
 
 <div class = 'question3'>
 	<p>Which choice is the correct way to rename a file?</p>
+	<div class ="question">
 	<input type='radio' name='answer3' value='1'>mv file1 file2</input>
 	<input type='radio' name='answer3' value='2'>rn file1 file 2</input>
 	<input type='radio' name='answer3' value='3'>rename file1 file2</input>
+	</div>
 </div>
 
 <div class = 'question4'>
-	<p>Which choice is the correct way move to a file?</p>
+	<h3>Which choice is the correct way move to a file?</h3>
+	<div class ="question">
 	<input type='radio' name='answer4' value='1'>vi file1 file2 newDestination</input>
 	<input type='radio' name='answer4' value='2'>mv file1 file2 newDestination</input>
 	<input type='radio' name='answer4' value='3'>move file1 file2 newDestination</input>
+	</div>
 </div>
 
 <div class = 'question5'>
-	<p>Which choice is the correct way to copy a file?</p>
+	<h3>Which choice is the correct way to copy a file?</h3>
+	<div class ="question">
 	<input type='radio' name='answer5' value='1'>cp file1 file2</input>
 	<input type='radio' name='answer5' value='2'>copy file1 file2</input>
 	<input type='radio' name='answer5' value='3'>vi file1 file2</input>
+	</div>
 </div>
 <button onclick="checkQuiz(x)">SUBMIT</button>
 	
