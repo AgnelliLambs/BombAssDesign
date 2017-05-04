c70712b438b808e1d52edd97f52059e79e9d437c
Tim Endersby <endersbyt@gmail.com>
2017-05-04 10:39:20 -0400

    Remove search bar.

--- assets/includes/navBar.php
+++ assets/includes/navBar.php
@@ -38,16 +38,6 @@
 			</li>
         <li><a href="<?php echo $path;?>glossary.php" <?php if ($heading == 'Glossary'){echo 'id="active"';}?>>GLOSSARY</a></li>
       </ul>
-		 <?php if($heading == 'Glossary'){
-			echo '
-		<div class="col-sm-3 col-md-3 pull-right">
-		<form class="navbar-form" role="search">
-		<div class="input-group">
-			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-			<dfiv class="input-group-btn">
-				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-			</div>
-			';} ?>
     </div>
   </div>
 </nav>
