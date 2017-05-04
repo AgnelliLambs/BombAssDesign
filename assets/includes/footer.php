4896591599d95696b98206ec63aa975d78cb4674
Tim Endersby <endersbyt@gmail.com>
2017-05-04 10:39:25 -0400

    Edit footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,37 +1,35 @@
-
-	<!-- </div>  end contentwrapper, remove/add if header php is different-->
-	<div id="Fullscreen"> </div>
-	<div id="dimBackground"></div>
+<!-- </div>  end contentwrapper, remove/add if header php is different-->
+<div id="Fullscreen"> </div>
+<div id="dimBackground"></div>
 </div>
-<div id="bottom"> 
+<div id="bottom">
 	<div id="bottomtext">
-		 <div class="row">
-		 
-      
-		<a href="<?php echo $path;?>about.php" style="color:#b4c1d1;">About Us</a><br>
-		<a href="<?php echo $path;?>citations.php" style="color:#b4c1d1;">Citations</a><br>
-		
-		<!--YouNix -->
-		<div class="col-xs-6">Site Owners:<br>
-		Copyright &copy; YouNix<br>
-		<div id="mobilePhoneLink">Phone: <a href="tel:18005559988" target="_blank">1-800-555-9988</a> <br></div>
-		<div id="desktopPhone">Phone: 1-800-555-9988<br></div>
-		Email: <a href="mailto:bombassdesign@gmail.com?subject=Website%20contact" target="_top">YouNix@gmail.com</a><br>
-		<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">123 Fake Str, Rochester, NY 14623</a>
+		<div class="row"> <a href="<?php echo $path;?>about.php" style="color:#b4c1d1;">About Us</a>
+			<br> <a href="<?php echo $path;?>citations.php" style="color:#b4c1d1;">Citations</a>
+			<br>
+			<!--YouNix -->
+			<div class="col-xs-6">Site Owners:
+				<br> Copyright &copy; YouNix
+				<br>
+				<div id="mobilePhoneLink">Phone: <a href="tel:18005559988" target="_blank">1-800-555-9988</a><br></div>
+				<div id="desktopPhone">Phone: 1-800-555-9988<br></div> 
+				Email: <a href="mailto:YouNix@gmail.com?subject=Website%20contact" target="_top">YouNix@gmail.com</a>
+				<br> <a href="http://maps.google.com/maps?q=123+Fake+Str,+Rochester,+NY+14623" target="_blank">123 Fake Str, Rochester, NY 14623</a> 
+			</div>
+			<!--Bomb Ass Design -->
+			<div class="col-xs-6">Site Design By: <br> 
+				Copyright &copy; Bomb Ass Design 2017 <br>
+				<div id="mobilePhoneLink">Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br>
+				</div>
+				<div id="desktopPhone">Phone: 1-800-555-8899 <br> </div> 
+				Email: <a href="mailto:bombassdesign@gmail.com?subject=Website%20contact" target="_top">bombassdesign@gmail.com</a> <br>
+				<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
+			</div>
 		</div>
-		
-		<!--Bomb Ass Design -->
-		<div class="col-xs-6">Site Design By:<br>
-		Copyright &copy; Bomb Ass Design 2017<br>
-		<div id="mobilePhoneLink">Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br></div>
-		<div id="desktopPhone">Phone: 1-800-555-8899<br></div>
-		Email: <a href="mailto:bombassdesign@gmail.com?subject=Website%20contact" target="_top">bombassdesign@gmail.com</a><br>
-		<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
-		</div>
-	</div> 
-	</div> 	
-</div> 
+	</div>
+</div>
 <script src="<?php echo $path;?>assets/js/fullscreen_photos.js"></script>
 <script src="<?php echo $path;?>assets/js/quizscripts.js"></script>
-</body> 
-</html> 
+</body>
+
+</html>
