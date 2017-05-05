c6ffcce5fe25720a432e7a3aad4b3ad64ab974f0
Tim Endersby <endersbyt@gmail.com>
2017-05-04 23:33:28 -0400

    Footer changes

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -4,27 +4,27 @@
 </div>
 <div id="bottom">
 	<div id="bottomtext">
-		<div class="row"> <a href="<?php echo $path;?>about.php" style="color:#b4c1d1;">About Us</a>
-			<br> <a href="<?php echo $path;?>citations.php" style="color:#b4c1d1;">Citations</a>
-			<br>
+		<div class="row">
 			<!--YouNix -->
 			<div class="col-xs-6">Site Owners:
-				<br> Copyright &copy; YouNix
+				<br> Copyright &copy; YouNix 2017
 				<br>
-				<div id="mobilePhoneLink">Phone: <a href="tel:18005559988" target="_blank">1-800-555-9988</a><br></div>
-				<div id="desktopPhone">Phone: 1-800-555-9988<br></div> 
-				Email: <a href="mailto:YouNix@gmail.com?subject=Website%20contact" target="_top">YouNix@gmail.com</a>
-				<br> <a href="http://maps.google.com/maps?q=123+Fake+Str,+Rochester,+NY+14623" target="_blank">123 Fake Str, Rochester, NY 14623</a> 
-			</div>
+				<div id="mobilePhoneLink">Phone: <a href="tel:18005559988" target="_blank">1-800-555-9988</a>
+					<br> </div>
+				<div id="desktopPhone">Phone: 1-800-555-9988
+					<br> </div> Email: <a href="mailto:YouNix@gmail.com?subject=Website%20contact" target="_top">YouNix@gmail.com</a>
+				<br> <a href="http://maps.google.com/maps?q=123+Fake+Str,+Rochester,+NY+14623" target="_blank">123 Fake Str, Rochester, NY 14623</a>
+				<br> <a href="<?php echo $path;?>citations.php" style="color:#b4c1d1;">Citations</a></div>
 			<!--Bomb Ass Design -->
-			<div class="col-xs-6">Site Design By: <br> 
-				Copyright &copy; Bomb Ass Design 2017 <br>
-				<div id="mobilePhoneLink">Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a> <br>
-				</div>
-				<div id="desktopPhone">Phone: 1-800-555-8899 <br> </div> 
-				Email: <a href="mailto:bombassdesign@gmail.com?subject=Website%20contact" target="_top">bombassdesign@gmail.com</a> <br>
-				<a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
-			</div>
+			<div class="col-xs-6">Site Design By:
+				<br> Copyright &copy; Bomb Ass Design 2017
+				<br>
+				<div id="mobilePhoneLink">Phone: <a href="tel:18005558899" target="_blank">1-800-555-8899</a>
+					<br> </div>
+				<div id="desktopPhone">Phone: 1-800-555-8899
+					<br> </div> Email: <a href="mailto:bombassdesign@gmail.com?subject=Website%20contact" target="_top">bombassdesign@gmail.com</a>
+				<br> <a href="http://maps.google.com/maps?q=1+Lomb+Memorial+Dr,+Rochester,+NY+14623" target="_blank">1 Lomb Memorial Dr, Rochester, NY 14623</a>
+				<br> <a href="<?php echo $path;?>about.php" style="color:#b4c1d1;">About Us</a></div>
 		</div>
 	</div>
 </div>
