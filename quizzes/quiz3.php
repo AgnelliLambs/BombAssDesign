81f5b485e1d7910f8141e57a4319c3d0b3799113
jkerz48 <jkerz48@gmail.com>
2017-05-10 14:58:39 -0400

    Update quiz3.php
    Fixed more labels and the button

--- quizzes/quiz3.php
+++ quizzes/quiz3.php
@@ -41,44 +41,43 @@
 			
 				<h3>Which choice is the correct way to rename a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer3' id="h" value='1'>
-					<label for="h">mv file1 file2</label>
+					<input type='radio' name='answer3' id="g" value='1'>
+					<label for="g">mv file1 file2</label>
 					<br/>
-					<input type='radio' name='answer3' id="i" value='2'>
-					<label for="i">rn file1 file 2</label>
+					<input type='radio' name='answer3' id="h" value='2'>
+					<label for="h">rn file1 file 2</label>
 					<br/>
-					<input type='radio' name='answer3' id="j" value='3'>
-					<label for="j">rename file1 file2</label>
+					<input type='radio' name='answer3' id="i" value='3'>
+					<label for="i">rename file1 file2</label>
 					<br/> 
 			</div>
 			
 				<h3>Which choice is the correct way move to a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer4' id="h" value='1'>
-					<label for="k">vi file1 file2 newDestination</label>
+					<input type='radio' name='answer4' id="j" value='1'>
+					<label for="j">vi file1 file2 newDestination</label>
 					<br/>
-					<input type='radio' name='answer4' id="i" value='2'>
-					<label for="l">mv file1 file2 newDestination</label>
+					<input type='radio' name='answer4' id="k" value='2'>
+					<label for="k">mv file1 file2 newDestination</label>
 					<br/>
-					<input type='radio' name='answer4' id="j" value='3'>
-					<label for="m">move file1 file2 newDestination</label>
+					<input type='radio' name='answer4' id="l" value='3'>
+					<label for="l">move file1 file2 newDestination</label>
 					<br/> 
 			</div>
 			
 				<h3>Which choice is the correct way to copy a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer5' id="k" value='1'>
-					<label for="n">cp file1 file2</label>
+					<input type='radio' name='answer5' id="m" value='1'>
+					<label for="m">cp file1 file2</label>
 					<br/>
-					<input type='radio' name='answer5' id="l" value='2'>
-					<label for="o">copy file1 file2</label>
+					<input type='radio' name='answer5' id="n" value='2'>
+					<label for="n">copy file1 file2</label>
 					<br/>
-					<input type='radio' name='answer5' id="m" value='3'>
-					<label for="p">vi file1 file2</label>
+					<input type='radio' name='answer5' id="o" value='3'>
+					<label for="o">vi file1 file2</label>
 					<br/> 
 			</div>
-			<button onclick="checkQuiz(x)">SUBMIT</button>
-		</form>
+			<input type="button" onclick="checkQuiz(3)" value="Submit"></form>
 	</div>
 	<!-- Footer PHP-->
 	<?php
