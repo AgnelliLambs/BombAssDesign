d128207dddf0a3d8ce390bf21ad1255a7946adf3
jkerz48 <jkerz48@gmail.com>
2017-05-10 15:54:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,7 @@
 				or you already have some experience, these tutorials can help you host your website for all 
 				the world to see.</p>
 			<p>YouNix was created by web developers, for web developers, and it's here to give you
-				all the tools you need to put your site onto a server. You can 
-				<a href="<?php echo $path?>tutorials/tutorial_home.php">start the tutorials</a>
+				all the tools you need to put your site onto a server. 
 		</div>
 		<div id="tutorialsStart">
 			<a id="tutorialsLink" href="tutorials/tutorial1.php">Start Tutorials</a> <!-- Goes to first tutorial -->
