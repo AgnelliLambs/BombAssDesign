fd46245f58951dd96e84d0516b7c038598486fbc
Tim Endersby <endersbyt@gmail.com>
2017-05-10 14:51:02 -0400

    create quiz 1

---
+++ quizzes/quiz1.php
@@ -0,0 +1,78 @@
+<!-- Quiz 5 -->
+<!-- B.A.D. Design -->
+<!-- Header -->
+<?php
+	$path = '../';
+	$title = 'Quiz 1';
+	$heading = 'Quiz';
+	include $path.'assets/includes/header.php';
+?>
+	<!-- Quiz -->
+	<div class="quiz">
+		<h2>File Permissions</h2>
+		<!-- Question 1 -->
+		<form>
+			<h3>Which program on windows allows you to SSH?</h3>
+			<div class="question">
+				<input type="radio" name="answer1" id="a" value="0">
+				<label for="a">Terminal</label>
+				<br>
+				<input type="radio" name="answer1" id="b" value="1">
+				<label for="b">PuTTY</label>
+				<br>
+				<input type="radio" name="answer1" id="c" value="2">
+				<label for="c">SSHer</label>
+				<br> </div>
+			<!-- Question 2 -->
+			<h3>What does SSH Stand for?</h3>
+			<div class="question">
+				<input type="radio" name="answer2" id="d" value="0">
+				<label for="d">Secure Site Program</label>
+				<br>
+				<input type="radio" name="answer2" id="e" value="1">
+				<label for="e">Saftey Shell Protocol</label>
+				<br>
+				<input type="radio" name="answer2" id="f" value="2">
+				<label for="f">Secure Shell Protocol</label>
+				<br> </div>
+			<!-- Question 3 -->
+			<h3>What command will connect to the server over SSH?</h3>
+			<div class="question">
+				<input type="radio" name="answer3" id="g" value="0">
+				<label for="g">USERNAME@HOSTNAME</label>
+				<br>
+				<input type="radio" name="answer3" id="h" value="1">
+				<label for="h">HOSTNAME@USERNAME</label>
+				<br>
+				<input type="radio" name="answer3" id="i" value="2">
+				<label for="i">USERNAME/HOSTNAME</label>
+				<br> </div>
+			<!-- Question 4 -->
+			<h3>What port does SSH use?</h3>
+			<div class="question">
+				<input type="radio" name="answer4" id="j" value="0">
+				<label for="j">21</label>
+				<br>
+				<input type="radio" name="answer4" id="k" value="1">
+				<label for="k">22</label>
+				<br>
+				<input type="radio" name="answer4" id="l" value="2">
+				<label for="l">23</label>
+				<br> </div>
+			<!-- Question 5 -->
+			<h3>What program on mac allows you to SSH?</h3>
+			<div class="question">
+				<input type="radio" name="answer5" id="m" value="0">
+				<label for="m">Terminal</label>
+				<br>
+				<input type="radio" name="answer5" id="n" value="1">
+				<label for="n">PuTTY</label>
+				<br>
+				<input type="radio" name="answer5" id="o" value="2">
+				<label for="o">SSHer</label>
+				<br> </div>
+			<div class="buttonHolder"><input type="button" onclick="checkQuiz(1)" value="Submit"></div> </form>
+	</div>
+	<?php
+	include $path.'assets/includes/footer.php';
+?>
