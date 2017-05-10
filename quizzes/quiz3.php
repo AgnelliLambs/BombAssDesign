2cd8201adaced6249a14f84cdb8915599528af0c
jkerz48 <jkerz48@gmail.com>
2017-05-10 14:35:02 -0400

    Update quiz3.php
    added ids

--- quizzes/quiz3.php
+++ quizzes/quiz3.php
@@ -14,51 +14,51 @@
 			<div class='question1'>
 				<h3>Question 1: Which choice is the correct way to create a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer1' value='1'>create file1
+					<input type='radio' name='answer1' id="a" value='1'>create file1
 					<br>
-					<input type='radio' name='answer1' value='2'>mkdir file1
+					<input type='radio' name='answer1' id="b" value='2'>mkdir file1
 					<br>
-					<input type='radio' name='answer1' value='3'>vi file1
+					<input type='radio' name='answer1' id="c" value='3'>vi file1
 					<br> </div>
 			</div>
 			<div class='question2'>
 				<div class="question">
 					<h3>Which choice is the correct way to delete a file?</h3>
-					<input type='radio' name='answer2' value='1'>rm file1
+					<input type='radio' name='answer2' id="e" value='1'>rm file1
 					<br>
-					<input type='radio' name='answer2' value='2'>rmdir file1
+					<input type='radio' name='answer2' id="f" value='2'>rmdir file1
 					<br>
-					<input type='radio' name='answer2' value='3'>delete file1
+					<input type='radio' name='answer2' id="g" value='3'>delete file1
 					<br> </div>
 			</div>
 			<div class='question3'>
 				<h3>Which choice is the correct way to rename a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer3' value='1'>mv file1 file2
+					<input type='radio' name='answer3' id="h" value='1'>mv file1 file2
 					<br>
-					<input type='radio' name='answer3' value='2'>rn file1 file 2
+					<input type='radio' name='answer3' id="i" value='2'>rn file1 file 2
 					<br>
-					<input type='radio' name='answer3' value='3'>rename file1 file2
+					<input type='radio' name='answer3' id="j" value='3'>rename file1 file2
 					<br> </div>
 			</div>
 			<div class='question4'>
 				<h3>Which choice is the correct way move to a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer4' value='1'>vi file1 file2 newDestination
+					<input type='radio' name='answer4' id="k" value='1'>vi file1 file2 newDestination
 					<br>
-					<input type='radio' name='answer4' value='2'>mv file1 file2 newDestination
+					<input type='radio' name='answer4' id="l" value='2'>mv file1 file2 newDestination
 					<br>
-					<input type='radio' name='answer4' value='3'>move file1 file2 newDestination
+					<input type='radio' name='answer4' id="m" value='3'>move file1 file2 newDestination
 					<br> </div>
 			</div>
 			<div class='question5'>
 				<h3>Which choice is the correct way to copy a file?</h3>
 				<div class="question">
-					<input type='radio' name='answer5' value='1'>cp file1 file2
+					<input type='radio' name='answer5' id="n" value='1'>cp file1 file2
 					<br>
-					<input type='radio' name='answer5' value='2'>copy file1 file2
+					<input type='radio' name='answer5' id="o" value='2'>copy file1 file2
 					<br>
-					<input type='radio' name='answer5' value='3'>vi file1 file2
+					<input type='radio' name='answer5' id="p" value='3'>vi file1 file2
 					<br> </div>
 			</div>
 			<button onclick="checkQuiz(x)">SUBMIT</button>
