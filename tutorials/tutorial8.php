c59960349c58e1b35cc443e77b86475d8c70c4a6
bmm723 <bmm7238@rit.edu>
2017-05-10 16:16:54 -0400

    Add files via upload

---
+++ tutorials/tutorial8.php
@@ -0,0 +1,17 @@
+<!-- Tutorial 4 -->
+<!-- B.A.D Design -->
+
+<!-- Header PHP -->
+ <?php
+  	$path = '../';
+  	$title = 'Tutorial 4';
+ 	$heading = 'Tutorial';
+  	include $path.'assets/includes/header.php';
+  ?>
+  <div class="tutorial">
+	<h1>Text Editors</h1>
+  </div>
+<!-- Footer PHP-->
+<?php
+	include $path.'assets/includes/footer.php';
+?>
