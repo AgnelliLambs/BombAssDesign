6604c44b6cebc28d5fe050c90f7e9449a5e0c867
rcb1205 <rcb1205@g.rit.edu>
2017-05-14 19:52:23 -0400

    Tutorial Homepage Style Changes

--- tutorials/tutorial_home.php
+++ tutorials/tutorial_home.php
@@ -11,30 +11,38 @@
 <div id='content'>
 	<h1>Tutorials</h1>
 	
-	<div class="leftAlign">
+	<div class="rightAlign">
 	<a href = "tutorial1.php"><div class = 'tut'><h3>SSH</h3><p>In this tutorial you will learn how
 		to ... </p></div></a>
-	<div class="rightAlign">
+	</div>
+	<div class="leftAlign">
 	<a href = "tutorial2.php"><div class = 'tut'><h3>Navigation</h3><p>In this tutorial you will learn how
 		to ... </p></div></a> 
-	<div class="leftAlign">
+	</div>
+	<div class="rightAlign">
 	<a href = "tutorial3.php"><div class = 'tut'><h3>Files</h3><p>In this tutorial you will learn how
-		to ... </p></div></a> 
-	<div class="rightAlign">
-	<a href = "tutorial4.php"><div class = 'tut'><h3>SFTP</h3><p>In this tutorial you will learn how
-		to ... </p></div></a> 
+		to ... </p></div></a>
+	</div>
 	<div class="leftAlign">
-	<a href = "tutorial5.php"><div class = 'tut'><h3>Permissions</h3><p>In this tutorial you will learn how
-		to ... </p></div></a> 
+	<a href = "tutorial4.php"><div class = 'tut'><h3>SFTP</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
 	<div class="rightAlign">
+	<a href = "tutorial5.php"><div class = 'tut'><h3>Permissions</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
+	<div class="leftAlign">	
 	<a href = "tutorial6.php"><div class = 'tut'><h3>Directories</h3><p>In this tutorial you will learn how
 		to ... </p></div></a> 
-	<div class="leftAlign">
+	</div>
+	<div class="rightAlign">
 	<a href = "tutorial7.php"><div class = 'tut'><h3>Apache</h3><p>In this tutorial you will learn how
 		to ... </p></div></a> 
-	<div class="rightAlign">
+	</div>
+	<div class="leftAlign">
 	<a href = "tutorial8.php"><div class = 'tut'><h3>Text Editors</h3><p>In this tutorial you will learn how
 		to ... </p></div></a> 
+	</div><!-- End of leftAlign div -->
 	<div class='median'></div>
 </div> <!-- end of content -->
 <!-- Footer PHP -->
