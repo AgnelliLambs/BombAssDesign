fd566cf0bb64ac934685619f385146bb7b706c23
bmm723 <bmm7238@rit.edu>
2017-05-14 19:42:47 -0400

    Update tutorial8.php

--- tutorials/tutorial8.php
+++ tutorials/tutorial8.php
@@ -1,15 +1,30 @@
-<!-- Tutorial 4 -->
+<!-- Tutorial 8 -->
 <!-- B.A.D Design -->
 
 <!-- Header PHP -->
  <?php
   	$path = '../';
-  	$title = 'Tutorial 4';
+  	$title = 'Tutorial 8';
  	$heading = 'Tutorial';
   	include $path.'assets/includes/header.php';
   ?>
   <div class="tutorial">
 	<h1>Text Editors</h1>
+	<p>
+		In this tutorial, we will be looking at two text editors; Vi, and Pico. 
+	</p>
+	<h2>Vi</h2>
+	<p>
+		To run Vi, input in the command line "vi" and then the filename that you want to edit.
+	</p>
+	<p class="code">$ vi &lt;filename&gt;</p>
+	<h3>Editing a File</h3>
+	<h2>Pico</h2>
+	<p>
+		To run Pico, input in the command line "pico" and then the filename that you want to edit.
+	</p>
+	<p class="code">$ pico &lt;filename&gt;</p>
+	<h3>Editing a File</h3>
   </div>
 <!-- Footer PHP-->
 <?php
