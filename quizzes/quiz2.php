6473e96bd7eebd3f7f80eb1c21ab24d8de2dac99
Joseph Agnelli <jxa4717@rit.edu>
2017-05-14 13:20:15 -0400

    wrote quiz2

---
+++ quizzes/quiz2.php
@@ -0,0 +1,91 @@
+<!-- Quiz 6 -->
+<!-- B.A.D Design -->
+
+
+<!-- Header PHP-->
+
+<?php
+	$path = '../';
+	$title = 'Quiz 2';
+	include $path.'assets/includes/header.php';
+?>
+		<!-- Quiz -->
+<div class="quiz">
+	<h2>Navigation Quiz</h2>
+	<form>
+
+	<h3>In UNIX, how do you find your current directory?</h3>
+	<div class ="question">
+	
+	<input type='radio' name='answer1' id='a' value='0'>
+	<label for="a">pwd</label>
+	<br/>
+	<input type='radio' name='answer1' id='b' value='1'>
+	<label for="b">print</label>
+	 <br/>
+	<input type='radio' name='answer1' id='c' value='2'>
+	<label for="c">whereami</label>
+	<br/>
+</div>
+
+
+	<h3>WHow do you list the files in your directory, using UNIX?</h3>
+	<div class ="question">
+	<input type='radio' name='answer2' id='d' value='0'>
+	<label for="d">dir</label>
+	<br/>
+	<input type='radio' name='answer2' id='e' value='1'>
+	<label for="e">ls</label>
+	<br/>
+	<input type='radio' name='answer2' id='f' value='2'>
+	<label for="f">listfiles</label>
+	<br/>
+</div>
+
+	<h3>How would you go up one directory?</h3>
+	<div class ="question">
+	<input type='radio' name='answer3' id='g' value='0'>
+	<label for="g">cd up</label>
+	<br/>
+	<input type='radio' name='answer3' id='h' value='1'>
+	<label for="h">cd ./</label>
+	<br/>
+	<input type='radio' name='answer3' id='i' value='2'>
+	<label for="i">cd ../</label>
+	<br/>
+</div>
+
+	<h3>How would you quickly navigate to the home directory?</h3>
+	<div class ="question">
+	<input type='radio' name='answer4' id='j' value='0'>
+	<label for="j">cd /</label>
+	<br/>
+	<input type='radio' name='answer4' id='k' value='1'>
+	<label for="k">cd .</label>
+	<br/>
+	<input type='radio' name='answer4' id='l' value='2'>
+	<label for="l">cd home</label>
+	<br/>
+</div>
+
+	<h3>How could you enter a subdirectory?</h3>
+	<div class ="question">
+	<input type='radio' name='answer5' id='m' value='0'>
+	<label for="m">cd <i>directory name</i></label>
+	<br/>
+	<input type='radio' name='answer5' id='n' value='1'>
+	<label for="n">goto child</label>
+	<br/>
+	<input type='radio' name='answer5' id='o' value='2'>
+	<label for="o">Impossibe, you can't</label>
+	<br/>
+</div>
+	<input type="button" onclick="checkQuiz(6)" value="Submit"></div>
+	</form>
+
+<!-- Footer PHP-->
+<?php
+	$path = '../';
+	$title = 'Quiz 6';
+	include $path.'assets/includes/footer.php';
+?>
