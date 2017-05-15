791760756c452054d27d1a501df4c48169fa44f5
bmm723 <bmm7238@rit.edu>
2017-05-14 20:41:42 -0400

    Update quiz8.php

--- quizzes/quiz8.php
+++ quizzes/quiz8.php
@@ -18,64 +18,64 @@
 		<div class="question">
 			<input type="radio" name="answer1" id="a" value="0">
 			<label for="a">:exit!<label>
-			</br>
+			<br>
 			<input type="radio" name="answer1" id="b" value="1">
 			<label for="b">:e!<label>
-			</br>
+			<br>
 			<input type="radio" name="answer1" id="c" value="2">
 			<label for="c">:q!<label> <!-- Correct answer -->
-			</br>
+			<br>
 			<input type="radio" name="answer1" id="d" value="3">
 			<label for="d">:quit!<label>
-			</br>
+			<br>
 		</div>
 		<h3>What command would you use to start editing a file with Vi?</h3>
 		<div class="question">
 			<input type="radio" name="answer2" id="e" value="0">
 			<label for="e">e<label>
-			</br>
+			<br>
 			<input type="radio" name="answer2" id="f" value="1">
 			<label for="f">i<label> <!-- Correct answer -->
-			</br>
+			<br>
 			<input type="radio" name="answer2" id="g" value="2">
 			<label for="g">edit<label>
-			</br>
+			<br>
 			<input type="radio" name="answer2" id="h" value="3">
 			<label for="h">insert<label>
-			</br>
+			<br>
 		</div>
 		<h3>Which of the following do you NOT need to know in order to open a text editor?</h3>
 		<div class="question">
 			<input type="radio" name="answe3" id="i" value="0">
 			<label for="i">transfer port<label> <!-- Correct answer -->
-			</br>
+			<br>
 			<input type="radio" name="answer3" id="j" value="1">
 			<label for="j">filename<label>
-			</br>
+			<br>
 			<input type="radio" name="answer3" id="k" value="2">
 			<label for="k">file path<label>
-			</br>
+			<br>
 			<input type="radio" name="answer3" id="l" value="3">
 			<label for="l">text editor name<label>
-			</br>
+			<br>
 		</div>
 		<h3>Vi and Pico can be used outside of the command line.</h3>
 		<div class="question">
 			<input type="radio" name="answer4" id="m" value="0">
 			<label for="m">true<label>
-			</br>
+			<br>
 			<input type="radio" name="answer4" id="n" value="1">
 			<label for="n">false<label> <!-- Correct answer -->
-			</br>
+			<br>
 		</div>
 		<h3>Any file type can be edited with Vi or Pico.</h3>
 		<div class="question">
 			<input type="radio" name="answe5" id="o" value="0">
 			<label for="o">true<label>
-			</br>
+			<br>
 			<input type="radio" name="answer5" id="p" value="1">
 			<label for="p">false<label> <!-- Correct answer -->
-			</br>
+			<br>
 		</div>
 		<div class="buttonHolder">
 		<div class="button" onclick="checkQuiz(8)" value="Submit"></div>
