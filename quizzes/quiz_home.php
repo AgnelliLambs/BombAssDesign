04b2ed6332088dbbaa1c370c0b16e2279bb6d62b
Joseph Agnelli <jxa4717@rit.edu>
2017-05-14 20:06:21 -0400

    fixed

--- quizzes/quiz_home.php
+++ quizzes/quiz_home.php
@@ -8,17 +8,42 @@
 	include $path.'assets/includes/header.php';
 ?>
 
+
 <div id='content'>
 	<h1>Quizzes</h1>
 	
-	<a href = "quiz1.php">SSH</a> 
-	<a href = "quiz2.php">Navigation</a> 
-	<a href = "quiz3.php">Files</a> 
-	<a href = "quiz4.php">SFTP</a> 
-	<a href = "quiz5.php">Permissions</a> 
-	<a href = "quiz6.php">Directories</a> 
-	<a href = "quiz7.php">Apache</a> 
-	<a href = "quiz8.php">Text Editors</a> 
+	<div class="lefttAlign">
+	<a href = "quiz1.php"><div class = 'tut'><h3>SSH</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
+	<div class="rightAlign">
+	<a href = "quiz2.php"><div class = 'tut'><h3>Navigation</h3><p>In this tutorial you will learn how
+		to ... </p></div></a> 
+	</div>
+	<div class="leftAlign">
+	<a href = "quiz3.php"><div class = 'tut'><h3>Files</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
+	<div class="rightAlign">
+	<a href = "quiz4.php"><div class = 'tut'><h3>SFTP</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
+	<div class="leftAlign">
+	<a href = "quiz5.php"><div class = 'tut'><h3>Permissions</h3><p>In this tutorial you will learn how
+		to ... </p></div></a>
+	</div>
+	<div class="rightAlign">	
+	<a href = "quiz6.php"><div class = 'tut'><h3>Directories</h3><p>In this tutorial you will learn how
+		to ... </p></div></a> 
+	</div>
+	<div class="leftAlign">
+	<a href = "quiz7.php"><div class = 'tut'><h3>Apache</h3><p>In this tutorial you will learn how
+		to ... </p></div></a> 
+	</div>
+	<div class="rightAlign">
+	<a href = "quiz8.php"><div class = 'tut'><h3>Text Editors</h3><p>In this tutorial you will learn how
+		to ... </p></div></a> 
+	</div><!-- End of Align divs -->
 	<div class='median'></div>
 </div> <!-- end of content -->
 <!-- Footer PHP -->
