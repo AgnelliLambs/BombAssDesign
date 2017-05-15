e0e17be1769a82b942872b1c5df24aae2545a793
bmm723 <bmm7238@rit.edu>
2017-05-15 19:07:32 -0400

    Update tutorial8.php

--- tutorials/tutorial8.php
+++ tutorials/tutorial8.php
@@ -10,6 +10,7 @@
   ?>
   <div class="tutorial">
 	<h1>Unix Text Editors</h1>
+	<!-- Intro to text editors -->
 	<p>
 		The text editors in Unix will allow you to make changes to the a file through command line.
 		In this tutorial, we will be looking at two text editors; vi, and pico. These two editors both
@@ -22,6 +23,7 @@
 		If the filename does not exist, a new file will be created with this name.
 	</p>
 	<p class="code">$ vi &lt;filename&gt;</p>
+	<!-- How to edit a file with vi -->
 	<h3>Editing a File</h3>
 	<p>
 		A file can be edited through command mode or insert mode. On start, vi will automatically
@@ -36,12 +38,14 @@
 		Using "k" will move your cursor up one line, and "j" will move it down one line. These
 		commands will not work upon entering insert mode.
 	</p>
+	<!-- How to save changes to a file -->
 	<h3>Saving Your Work</h3>
 	<p>
 		Changes cannot be save while in insert mode. To save a change in command
 		mode, enter the command ":w". If you quit without saving first, your changes
 		will be lost.
 	</p>
+	<!-- How to exit as well as save and exit -->
 	<h3>Exiting the Editor</h3>
 	<p>
 		To exit insert mode, simply press the escape key on your keyboard. To exit vi altogether,
@@ -59,12 +63,13 @@
 		a new file will be made.
 	</p>
 	<p class="code">$ pico &lt;filename&gt;</p>
-	<h3>Usin Pico</h3>
+	<h3>Using Pico</h3>
 	<p>
 		As pico has no command mode, it will automatically open your file
 		on insert mode. The command "^G" will bring up a list of all the
 		commands that can be used in pico.
 	</p>
+	<!-- Image shows all pico commands -->
 	<img src="<?php echo $path;?>assets/imgs/tutorial8/picocommands.jpg" alt="Pico commands">
   </div>
 <!-- Footer PHP-->
