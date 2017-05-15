3b3888a94796c011c0a8af1f12095dd454af1802
rcb1205 <rcb1205@g.rit.edu>
2017-05-14 20:49:08 -0400

    Final fix tutorial home

--- tutorials/tutorial_home.php
+++ tutorials/tutorial_home.php
@@ -9,42 +9,52 @@
 ?>
 
 <div id='content'>
-	<h1>Tutorials</h1>
-	
-	<div class="lefttAlign">
-	<a href = "tutorial1.php"><div class = 'tut'>SSH<p>In this tutorial you will learn how
-		to securely connect to a remote computer. </p></div></a>
-	</div>
-	<div class="rightAlign">
-	<a href = "tutorial2.php"><div class = 'tut'>Navigation<p>In this tutorial you will learn how
-		to navigate the linux file system. </p></div></a> 
-	</div>
-	<div class="leftAlign">
-	<a href = "tutorial3.php"><div class = 'tut'>Files<p>In this tutorial you will learn how
-		to manipulate files on the computer. </p></div></a>
-	</div>
-	<div class="rightAlign">
-	<a href = "tutorial4.php"><div class = 'tut'>SFTP<p>In this tutorial you will learn how
-		to securely connect to a remote server. </p></div></a>
-	</div>
-	<div class="leftAlign">
-	<a href = "tutorial5.php"><div class = 'tut'>Permissions<p>In this tutorial you will learn how
-		to view and change file permissions. </p></div></a>
-	</div>
-	<div class="rightAlign">	
-	<a href = "tutorial6.php"><div class = 'tut'>Directories<p>In this tutorial you will learn how
-		to manipulate directorie within the file system. </p></div></a> 
-	</div>
-	<div class="leftAlign">
-	<a href = "tutorial7.php"><div class = 'tut'>Apache<p>In this tutorial you will learn how
-		to download and install Apache. </p></div></a> 
-	</div>
-	<div class="rightAlign">
-	<a href = "tutorial8.php"><div class = 'tut'>Text Editors<p>In this tutorial you will learn how
-		to use the text editors within linux. </p></div></a> 
-	</div><!-- End of Align divs -->
-	<div class='median'></div>
+		<h1>Tutorials</h1>
+	<div id='leftContent'>
+		
+		<div class="lefttAlign">
+		<a href = "tutorial1.php"><div class = 'tut'>SSH<p>In this tutorial you will learn how
+			to securely connect to a remote computer. </p></div></a>
+		</div>
+
+		<div class="leftAlign">
+		<a href = "tutorial3.php"><div class = 'tut'>Files<p>In this tutorial you will learn how
+			to manipulate files on the computer. </p></div></a>
+		</div>
+
+		<div class="leftAlign">
+		<a href = "tutorial5.php"><div class = 'tut'>Permissions<p>In this tutorial you will learn how
+			to view and change file permissions. </p></div></a>
+		</div>
+
+		<div class="leftAlign">
+		<a href = "tutorial7.php"><div class = 'tut'>Apache<p>In this tutorial you will learn how
+			to download and install Apache. </p></div></a> 
+		</div>
+	</div><!-- end of leftContent -->
+	<div id="rightContent">
+		<div class="rightAlign">
+		<a href = "tutorial2.php"><div class = 'tut'>Navigation<p>In this tutorial you will learn how
+			to navigate the linux file system. </p></div></a> 
+		</div>
+		
+		<div class="rightAlign">
+		<a href = "tutorial4.php"><div class = 'tut'>SFTP<p>In this tutorial you will learn how
+			to securely connect to a remote server. </p></div></a>
+		</div>
+		
+		<div class="rightAlign">	
+		<a href = "tutorial6.php"><div class = 'tut'>Directories<p>In this tutorial you will learn how
+			to manipulate directorie within the file system. </p></div></a> 
+		</div>
+		
+		<div class="rightAlign">
+		<a href = "tutorial8.php"><div class = 'tut'>Text Editors<p>In this tutorial you will learn how
+			to use the text editors within linux. </p></div></a> 
+		</div>
+	</div> <!-- end of rightContent -->
 </div> <!-- end of content -->
+	<div class='median'></div>
 <!-- Footer PHP -->
 <?php
 	include $path.'assets/includes/footer.php';
