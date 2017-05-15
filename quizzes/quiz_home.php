3e2e2491e127b8e3fd3416068969a484ccde7c24
Joseph Agnelli <jxa4717@rit.edu>
2017-05-14 20:23:37 -0400

    here you go Cap'n tim!

--- quizzes/quiz_home.php
+++ quizzes/quiz_home.php
@@ -13,35 +13,35 @@
 	<h1>Quizzes</h1>
 	
 	<div class="lefttAlign">
-	<a href = "quiz1.php"><div class = 'tut'><h3>SSH</h3><p>In this tutorial you will learn how
+	<a href = "quiz1.php"><div class = 'tut'>SSH<p>In this tutorial you will learn how
 		to ... </p></div></a>
 	</div>
 	<div class="rightAlign">
-	<a href = "quiz2.php"><div class = 'tut'><h3>Navigation</h3><p>In this tutorial you will learn how
+	<a href = "quiz2.php"><div class = 'tut'>Navigation<p>In this tutorial you will learn how
 		to ... </p></div></a> 
 	</div>
 	<div class="leftAlign">
-	<a href = "quiz3.php"><div class = 'tut'><h3>Files</h3><p>In this tutorial you will learn how
+	<a href = "quiz3.php"><div class = 'tut'>Files<p>In this tutorial you will learn how
 		to ... </p></div></a>
 	</div>
 	<div class="rightAlign">
-	<a href = "quiz4.php"><div class = 'tut'><h3>SFTP</h3><p>In this tutorial you will learn how
+	<a href = "quiz4.php"><div class = 'tut'>SFTP<p>In this tutorial you will learn how
 		to ... </p></div></a>
 	</div>
 	<div class="leftAlign">
-	<a href = "quiz5.php"><div class = 'tut'><h3>Permissions</h3><p>In this tutorial you will learn how
+	<a href = "quiz5.php"><div class = 'tut'>Permissions<p>In this tutorial you will learn how
 		to ... </p></div></a>
 	</div>
 	<div class="rightAlign">	
-	<a href = "quiz6.php"><div class = 'tut'><h3>Directories</h3><p>In this tutorial you will learn how
+	<a href = "quiz6.php"><div class = 'tut'>Directories<p>In this tutorial you will learn how
 		to ... </p></div></a> 
 	</div>
 	<div class="leftAlign">
-	<a href = "quiz7.php"><div class = 'tut'><h3>Apache</h3><p>In this tutorial you will learn how
+	<a href = "quiz7.php"><div class = 'tut'>Apache<p>In this tutorial you will learn how
 		to ... </p></div></a> 
 	</div>
 	<div class="rightAlign">
-	<a href = "quiz8.php"><div class = 'tut'><h3>Text Editors</h3><p>In this tutorial you will learn how
+	<a href = "quiz8.php"><div class = 'tut'>Text Editors<p>In this tutorial you will learn how
 		to ... </p></div></a> 
 	</div><!-- End of Align divs -->
 	<div class='median'></div>
--- tutorials/tutorial_home.php
+++ tutorials/tutorial_home.php
@@ -12,35 +12,35 @@
 	<h1>Tutorials</h1>
 	
 	<div class="lefttAlign">
-	<a href = "tutorial1.php"><div class = 'tut'><h3>SSH</h3><p>In this tutorial you will learn how
+	<a href = "tutorial1.php"><div class = 'tut'>SSH<p>In this tutorial you will learn how
 		to securely connect to a remote computer. </p></div></a>
 	</div>
 	<div class="rightAlign">
-	<a href = "tutorial2.php"><div class = 'tut'><h3>Navigation</h3><p>In this tutorial you will learn how
+	<a href = "tutorial2.php"><div class = 'tut'>Navigation<p>In this tutorial you will learn how
 		to navigate the linux file system. </p></div></a> 
 	</div>
 	<div class="leftAlign">
-	<a href = "tutorial3.php"><div class = 'tut'><h3>Files</h3><p>In this tutorial you will learn how
+	<a href = "tutorial3.php"><div class = 'tut'>Files<p>In this tutorial you will learn how
 		to manipulate files on the computer. </p></div></a>
 	</div>
 	<div class="rightAlign">
-	<a href = "tutorial4.php"><div class = 'tut'><h3>SFTP</h3><p>In this tutorial you will learn how
+	<a href = "tutorial4.php"><div class = 'tut'>SFTP<p>In this tutorial you will learn how
 		to securely connect to a remote server. </p></div></a>
 	</div>
 	<div class="leftAlign">
-	<a href = "tutorial5.php"><div class = 'tut'><h3>Permissions</h3><p>In this tutorial you will learn how
+	<a href = "tutorial5.php"><div class = 'tut'>Permissions<p>In this tutorial you will learn how
 		to view and change file permissions. </p></div></a>
 	</div>
 	<div class="rightAlign">	
-	<a href = "tutorial6.php"><div class = 'tut'><h3>Directories</h3><p>In this tutorial you will learn how
+	<a href = "tutorial6.php"><div class = 'tut'>Directories<p>In this tutorial you will learn how
 		to manipulate directorie within the file system. </p></div></a> 
 	</div>
 	<div class="leftAlign">
-	<a href = "tutorial7.php"><div class = 'tut'><h3>Apache</h3><p>In this tutorial you will learn how
+	<a href = "tutorial7.php"><div class = 'tut'>Apache<p>In this tutorial you will learn how
 		to download and install Apache. </p></div></a> 
 	</div>
 	<div class="rightAlign">
-	<a href = "tutorial8.php"><div class = 'tut'><h3>Text Editors</h3><p>In this tutorial you will learn how
+	<a href = "tutorial8.php"><div class = 'tut'>Text Editors<p>In this tutorial you will learn how
 		to use the text editors within linux. </p></div></a> 
 	</div><!-- End of Align divs -->
 	<div class='median'></div>
