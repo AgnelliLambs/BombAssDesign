41236bc9d46a974186a0b16e7653137eaa1aa8f3
jkerz48 <jkerz48@gmail.com>
2017-05-16 19:14:36 -0400

    Update quiz5.php

--- quizzes/quiz5.php
+++ quizzes/quiz5.php
@@ -74,6 +74,21 @@
 				<br> </div>
 			<div class="buttonHolder"><input type="button" onclick="checkQuiz(5)" value="Submit"></div> </form>
 	</div>
+
+	<div class="row">
+		<div class="col-md-4 col-sm-4 centered">
+			<div class="circleButton">
+				<a id='quizLink' href="<?php echo $path;?>quizzes/quiz4.php">Previous Quiz</a>
+			</div></div>
+		<div class="col-md-4 col-sm-4 centered">
+			<div class="circleButton">
+			<a id='quizLink' href="<?php echo $path;?>tutorial/tutorial5.php">Go to Tutorial</a>
+			</div></div>
+		<div class="col-md-4 col-sm-4 centered">
+			<div class="circleButton">
+				<a id='quizLink' href="<?php echo $path;?>quizzes/quiz6.php">Next Quiz</a>
+			</div></div>
+</div>
 	<?php
 	include $path.'assets/includes/footer.php';
 ?>
