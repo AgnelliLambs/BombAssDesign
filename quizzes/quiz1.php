aa19b410770f99d483d1d531847f7c3999554a3a
jkerz48 <jkerz48@gmail.com>
2017-05-16 19:20:49 -0400

    Update quiz1.php

--- quizzes/quiz1.php
+++ quizzes/quiz1.php
@@ -80,7 +80,7 @@
 			</div></div>
 		<div class="col-md-4 col-sm-4 centered">
 			<div class="circleButton">
-			<a id='quizLink' href="<?php echo $path;?>tutorial/tutorial1.php">Go to Tutorial</a>
+			<a id='quizLink' href="<?php echo $path;?>tutorials/tutorial1.php">Go to Tutorial</a>
 			</div></div>
 		<div class="col-md-4 col-sm-4 centered">
 			<div class="circleButton">
