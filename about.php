772fe6191d0f0d1ff8c57332add4a56c43c03128
jkerz48 <jkerz48@gmail.com>
2017-05-16 20:37:59 -0400

    Update about.php
    added content stolen from personal pages

--- about.php
+++ about.php
@@ -8,6 +8,45 @@
  
 <h1>About us</h1>
 
+<h2>Joseph Agnelli</h2>
+<p>Hi! I'm Joe, I was born in New Hampshire on June 4, 1998 and have lived in various towns all throughout it for my entire life. 
+	I decided to come to RIT to study Human Centered Computing because it combined a lot of my interests into one major. 
+	I like to listen to music (my favorite artist is George Watsky) 
+	I also like to do card tricks and, make stuff. 
+</p>
+
+<h2>Ryan Brown</h2>
+<p>
+	I am Ryan Brown and I grew up locally in West-Henrietta. 
+	I am Twenty-One years old and am currently on my fourth semester as a CIT major. 
+	I am a full time student at the Rochester Institute of Technology and I work around 8 to 10 hours a week for the MMET/PS department. I went to Monroe Community College for a year after graduating High-School in 2013, transfering to RIT still studying as a Biology major. 
+	After a two semesters at RIT I transfered majors to CIT and have loved the courses ever since.
+</p>
+
+
+<h2> Tim Endersby</h2>
+<p>
+	I'm a second year CIT major. 
+	My hobbies include photography, video, and working with kids. 
+	I've spend the past few summers working at a summer camp as a climbing and media counselor. 
+</p>
+
+<h2> Joe Kerznowski</h2>
+<p>
+	I'm a third year CIT major from Syracuse, NY.
+	In my free time I enjoy running, watching movies, and hiking
+	I also really like the shows Game of Thrones and Rick and Morty.
+</p>
+
+<h2> Brie McIntosh</h2>
+<p>
+	My name is Brie McIntosh and I am from Newark, Delaware.
+	I am a second year Web 	&amp; Mobile Computing major here at RIT.
+	Prior to coming to RIT, I had no computing experience at all so everything I have learned has been thanks to my professors these past two years.
+</p>
+
+
+
 <!-- Footer PHP -->
 <?php
 	include $path.'assets/includes/footer.php';
